0993090042f60b6ab8a8dac6542bcd31c1853914
steven-maina <stevenmaina17@gmail.com>
2024-01-04 00:19:37 +0300

    edits

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
-@extends('layouts.admin')
-@section('page_title', 'My Dashboard')
+@extends('layouts.app')
+{{--@section('page_title', 'My Dashboard')--}}
 @section('content')
-{{-- <div class="container">
+ <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -33,88 +33,89 @@
             window.location.href = "{{ route('enrollments.index') }}"; // Redirect to student page
         });
     });
-</script> --}}
-<div class="container">
-@if(Auth::user()->role=='teacher' || (Auth::user()->role=='admin'))
-       <div class="row">
-           <div class="col-sm-6 col-xl-3">
-               <div class="card card-body bg-blue-400 has-bg-image">
-                   <div class="media">
-                       <div class="media-body">
-                           <h3 class="mb-0">{{ $users->where('role', 'student')->count() }}</h3>
-                           <span class="text-uppercase font-size-xs font-weight-bold">Total Students</span>
-                       </div>
+</script>
+{{--<div class="container">--}}
+{{--@if(Auth::user()->role=='teacher' || (Auth::user()->role=='admin'))--}}
+{{--       <div class="row">--}}
+{{--           <div class="col-sm-6 col-xl-3">--}}
+{{--               <div class="card card-body bg-blue-400 has-bg-image">--}}
+{{--                   <div class="media">--}}
+{{--                       <div class="media-body">--}}
+{{--                           <h3 class="mb-0">{{ $users->where('role', 'student')->count() }}</h3>--}}
+{{--                           <span class="text-uppercase font-size-xs font-weight-bold">Total Students</span>--}}
+{{--                       </div>--}}
 
-                       <div class="ml-3 align-self-center">
-                           <i class="icon-users4 icon-3x opacity-75"></i>
-                       </div>
-                   </div>
-               </div>
-           </div>
-           @endif
-           @if((Auth::user()->role=='admin'))
-           <div class="col-sm-6 col-xl-3">
-               <div class="card card-body bg-danger-400 has-bg-image">
-                   <div class="media">
-                       <div class="media-body">
-                           <h3 class="mb-0">{{ $users->where('role', 'teacher')->count() }}</h3>
-                           <span class="text-uppercase font-size-xs">Total Teachers</span>
-                       </div>
+{{--                       <div class="ml-3 align-self-center">--}}
+{{--                           <i class="icon-users4 icon-3x opacity-75"></i>--}}
+{{--                       </div>--}}
+{{--                   </div>--}}
+{{--               </div>--}}
+{{--           </div>--}}
+{{--           @endif--}}
+{{--           @if((Auth::user()->role=='admin'))--}}
+{{--           <div class="col-sm-6 col-xl-3">--}}
+{{--               <div class="card card-body bg-danger-400 has-bg-image">--}}
+{{--                   <div class="media">--}}
+{{--                       <div class="media-body">--}}
+{{--                           <h3 class="mb-0">{{ $users->where('role', 'teacher')->count() }}</h3>--}}
+{{--                           <span class="text-uppercase font-size-xs">Total Teachers</span>--}}
+{{--                       </div>--}}
 
-                       <div class="ml-3 align-self-center">
-                           <i class="icon-users2 icon-3x opacity-75"></i>
-                       </div>
-                   </div>
-               </div>
-           </div>
-           <div class="col-sm-6 col-xl-3">
-               <div class="card card-body bg-success-400 has-bg-image">
-                   <div class="media">
-                       <div class="mr-3 align-self-center">
-                           <i class="icon-pointer icon-3x opacity-75"></i>
-                       </div>
+{{--                       <div class="ml-3 align-self-center">--}}
+{{--                           <i class="icon-users2 icon-3x opacity-75"></i>--}}
+{{--                       </div>--}}
+{{--                   </div>--}}
+{{--               </div>--}}
+{{--           </div>--}}
+{{--           <div class="col-sm-6 col-xl-3">--}}
+{{--               <div class="card card-body bg-success-400 has-bg-image">--}}
+{{--                   <div class="media">--}}
+{{--                       <div class="mr-3 align-self-center">--}}
+{{--                           <i class="icon-pointer icon-3x opacity-75"></i>--}}
+{{--                       </div>--}}
 
-                       <div class="media-body text-right">
-                           <h3 class="mb-0">{{ $users->where('admin', 'admin')->count() }}</h3>
-                           <span class="text-uppercase font-size-xs">Total Administrators</span>
-                       </div>
-                   </div>
-               </div>
-           </div>
-       </div>
-    @endif
-    @if((Auth::user()->role=='student'))
-    <div class="col-sm-6 col-xl-3">
-        <div class="card card-body bg-danger-400 has-bg-image">
-            <div class="media">
-                <div class="media-body">
-                    <h3 class="mb-0">{{ $courses->count() }}</h3>
-                    <span class="text-uppercase font-size-xs">Available Courses</span>
-                </div>
+{{--                       <div class="media-body text-right">--}}
+{{--                           <h3 class="mb-0">{{ $users->where('admin', 'admin')->count() }}</h3>--}}
+{{--                           <span class="text-uppercase font-size-xs">Total Administrators</span>--}}
+{{--                       </div>--}}
+{{--                   </div>--}}
+{{--               </div>--}}
+{{--           </div>--}}
+{{--       </div>--}}
+{{--    @endif--}}
+{{--    @if((Auth::user()->role=='student'))--}}
+{{--    <div class="col-sm-6 col-xl-3">--}}
+{{--        <div class="card card-body bg-danger-400 has-bg-image">--}}
+{{--            <div class="media">--}}
+{{--                <div class="media-body">--}}
+{{--                    <h3 class="mb-0">{{ $courses->count() }}</h3>--}}
+{{--                    <span class="text-uppercase font-size-xs">Available Courses</span>--}}
+{{--                </div>--}}
 
-                <div class="ml-3 align-self-center">
-                    <i class="icon-users2 icon-3x opacity-75"></i>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-sm-6 col-xl-3">
-        <div class="card card-body bg-success-400 has-bg-image">
-            <div class="media">
-                <div class="mr-3 align-self-center">
-                    <i class="icon-pointer icon-3x opacity-75"></i>
-                </div>
+{{--                <div class="ml-3 align-self-center">--}}
+{{--                    <i class="icon-users2 icon-3x opacity-75"></i>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--    <div class="col-sm-6 col-xl-3">--}}
+{{--        <div class="card card-body bg-success-400 has-bg-image">--}}
+{{--            <div class="media">--}}
+{{--                <div class="mr-3 align-self-center">--}}
+{{--                    <i class="icon-pointer icon-3x opacity-75"></i>--}}
+{{--                </div>--}}
 
-                <div class="media-body text-right">
-                    <h3 class="mb-0">{{ $enrollments->count() }}</h3>
-                    <span class="text-uppercase font-size-xs">Current Enrollments</span>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endif
+{{--                <div class="media-body text-right">--}}
+{{--                    <h3 class="mb-0">{{ $enrollments->count() }}</h3>--}}
+{{--                    <span class="text-uppercase font-size-xs">Current Enrollments</span>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--</div>--}}
+{{--@endif--}}
 
-</div>
+{{--</div>--}}
+
 
 @endsection
